4fa44d76295e5e967921881d62fa2ead2593570a
Ross Schlaikjer <ross@schlaikjer.net>
2017-03-26 17:26:59 -0400

    Finish the parsing code

--- src/ofxconnect_parser.erl
+++ src/ofxconnect_parser.erl
@@ -1,7 +1,15 @@
 -module(ofxconnect_parser).
 -include("include/records.hrl").
--compile(export_all).
+-export([
+    marshal/1,
+    unmarshal/1
+]).
 
+%%
+%% Public methods
+%%
+
+% Dump an OFX tree out to a deep IO list
 marshal(#ofx_node{name=Name, children=Children}) ->
     [
      "<", Name, ">",
@@ -11,19 +19,51 @@ marshal(#ofx_node{name=Name, children=Children}) ->
 marshal(#ofx_leaf{name=Name, value=Value}) ->
     ["<", Name, ">", Value].
 
+% Read a string into an OFX tree
+unmarshal(Text) ->
+    parse(lex(Text)).
+
+
+%%
+%% Internal methods
+%%
+
+% Lexes a string into a list of tags. The tags can be fed to parse_node
+% to generate an OFX document
 lex(Ofx) ->
-    leex:file('ofx.xrl'),
-    compile:file("ofx.erl"),
+    leex:file('ofxconnect_leex.xrl'),
+    compile:file("ofxconnect_leex.erl"),
     {ok, Tags, _} = ofx:string(Ofx),
     Tags.
 
+% Parses a list of tags into an OFX data tree.
+% Will error out in there are tokens that cannot be parsed as part of the tree.
 parse(Tags) ->
-    do_parse(Tags).
+    {Tree, Unparsed} = parse_node(Tags),
+    [] = Unparsed,
+    Tree.
 
-do_parse([{opentag, Tag}|[{string, Value}|Tags]]) ->
-    #ofx_leaf{name=Tag, value=Value};
-do_parse([{opentag, Tag}|[{opentag, ChildTag}|Tags]]) ->
-    #ofx_node{
-       name=Tag,
-       children=[do_parse([{opentag, ChildTag}|Tags])]
-    }.
+% Parse a single OFX node from tokens.
+% Returns the node, and any unused tokens.
+parse_node([{opentag, Tag}|[{string, Value}|Tags]]) ->
+    {#ofx_leaf{name=Tag, value=Value}, Tags};
+parse_node([{opentag, Tag}|Tags]) ->
+    {Children, Tags2} = parse_node_list(Tag, Tags),
+    {#ofx_node{name=Tag,children=Children}, Tags2}.
+
+% Convenience method for parse_node_list/3.
+parse_node_list(EndTag, Tags) ->
+    parse_node_list(EndTag, Tags, []).
+
+% Parses a list of child nodes. Stops parsing when a {closetag, } tuple is found
+% with a name matching the EndTag.
+parse_node_list(_EndTag, [], Nodes) ->
+    Nodes;
+parse_node_list(EndTag, [Tag|Tags], Nodes) ->
+    {Node, Tags2} = parse_node([Tag|Tags]),
+    case hd(Tags2) of
+        {closetag, EndTag} ->
+            {[Node|Nodes], tl(Tags2)};
+        _ ->
+            parse_node_list(EndTag, Tags2, [Node|Nodes])
+    end.
