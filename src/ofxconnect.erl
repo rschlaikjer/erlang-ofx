36740649d63a70371eaa7f86baf29807f9f73d5e
Ross Schlaikjer <ross@schlaikjer.net>
2017-03-26 20:18:15 -0400

    Update request code

--- src/ofxconnect.erl
+++ src/ofxconnect.erl
@@ -1,12 +1,20 @@
 -module(ofxconnect).
 -include("include/records.hrl").
--compile(export_all).
+-define(CLIENTUID, "b2f82e64-ca94-45e1-b947-b9007cd3cd08").
+
+-export([
+    get_header/0,
+    get_bank/9,
+    get_ccard/7,
+    ofx_request/2
+]).
+
 
 get_header() ->
     Headers =  [
         {"OFXHEADER", "100"},
         {"DATA", "OFXSGML"},
-        {"VERSION", "102"},
+        {"VERSION", "103"},
         {"SECURITY", "NONE"},
         {"ENCODING", "USASCII"},
         {"CHARSET", "1252"},
@@ -14,7 +22,7 @@ get_header() ->
         {"OLDFILEUID", "NONE"},
         {"NEWFILEUID", uuid:to_string(uuid:uuid4())}
     ],
-    string:join([[K, ":", V] || {K, V} <- Headers], "\n").
+    string:join([[K, ":", V] || {K, V} <- Headers], "\r\n").
 
 get_fi(Org, Fid) ->
     #ofx_node{name="FI", children=[
@@ -37,11 +45,23 @@ get_signon(Username, Pass, Org, Fid) ->
             #ofx_leaf{name="LANGUAGE", value="ENG"},
             get_fi(Org, Fid),
             #ofx_leaf{name="APPID", value="QWIN"},
-            #ofx_leaf{name="APPVER", value="2200"}
+            #ofx_leaf{name="APPVER", value="2200"},
+            #ofx_leaf{name="CLIENTUID", value=?CLIENTUID}
         ]}
     ]}.
 
-get_bankmessage_request(BankId, AccountId, AccountType, TimeStart, TimeEnd) ->
+list_accounts() ->
+    #ofx_node{name="SIGNUPMSGSRQV1", children=[
+        #ofx_node{name="ACCTINFOTRNRQ", children=[
+            #ofx_leaf{name="TRNUID", value=uuid:to_string(uuid:uuid4())},
+            #ofx_leaf{name="CLTCOOKIE", value="1"},
+            #ofx_node{name="ACCTINFORQ", children=[
+                #ofx_leaf{name="DTACCTUP", value="19691231"}
+            ]}
+        ]}
+    ]}.
+
+bank_message(BankId, AccountId, AccountType, TimeStart, TimeEnd) ->
     #ofx_node{name="BANKMSGSRQV1", children=[
         #ofx_node{name="STMTTRNRQ", children=[
             #ofx_leaf{name="TRNUID", value=uuid:to_string(uuid:uuid4())},
@@ -61,35 +81,56 @@ get_bankmessage_request(BankId, AccountId, AccountType, TimeStart, TimeEnd) ->
         ]}
     ]}.
 
-generate_request(Username, Pass, Org, Fid, BankId, AccountId, AccountType, TimeStart, TimeEnd) ->
+ccard_message(AccountId, TimeStart, TimeEnd) ->
+    #ofx_node{name="CREDITCARDMSGSRQV1", children=[
+        #ofx_node{name="CCSTMTTRNRQ", children=[
+            #ofx_leaf{name="TRNUID", value=uuid:to_string(uuid:uuid4())},
+            #ofx_leaf{name="CLTCOOKIE", value="1"},
+            #ofx_node{name="CCSTMTRQ", children=[
+                #ofx_node{name="CCACCTFROM", children=[
+                    #ofx_leaf{name="ACCTID", value=AccountId}
+                ]},
+                #ofx_node{name="INCTRAN", children=[
+                    #ofx_leaf{name="DTSTART", value=TimeStart},
+                    #ofx_leaf{name="DTEND", value=TimeEnd},
+                    #ofx_leaf{name="INCLUDE", value="Y"}
+                ]}
+            ]}
+        ]}
+    ]}.
+
+get_bank(Username, Pass, Org, Fid, BankId, AccountId, AccountType, TimeStart, TimeEnd) ->
     OfxData = ofxconnect_parser:marshal(
          #ofx_node{
             name="OFX",
             children=[
                get_signon(Username, Pass, Org, Fid),
-               get_bankmessage_request(BankId, AccountId, AccountType, TimeStart, TimeEnd)
+               bank_message(BankId, AccountId, AccountType, TimeStart, TimeEnd)
             ]
            }),
-    Body = lists:flatten(
-      [
-       get_header(),
-       "\n\n",
-       OfxData
-      ]),
-    io:format("Body: ~s~n", [Body]),
-    Body.
+    [get_header(), "\n\n", OfxData].
+
+get_ccard(Username, Pass, Org, Fid, AccountId, TimeStart, TimeEnd) ->
+    OfxData = ofxconnect_parser:marshal(
+         #ofx_node{
+            name="OFX",
+            children=[
+               get_signon(Username, Pass, Org, Fid),
+               ccard_message(AccountId, TimeStart, TimeEnd)
+            ]
+           }),
+    [get_header(), "\n\n", OfxData].
 
 ofx_request(Url, Body) ->
-    Headers = [],
+    Headers = [
+        {"user-agent", "InetClntApp/3.0"},
+        {"connection", "close"},
+        {"accept", "*/*, application/x-ofx"}
+    ],
     ContentType = "application/x-ofx",
-    httpc:request(
+    {ok, {_, _, Resp}} = httpc:request(
         post,
-        {Url, Headers, ContentType, Body},
+        {Url, Headers, ContentType, lists:flatten(Body)},
         [], []
-    ).
-
-get_transactions() ->
-    Header = get_header(),
-
-    ok.
-
+    ),
+    Resp.
