57e484d88720bcf2601fadebc2a0a69af39d5344
Ross Schlaikjer <ross@schlaikjer.net>
2017-03-27 19:15:01 -0400

    Add methods for manipulating the OFX tree

---
+++ src/ofx_tree.erl
@@ -0,0 +1,28 @@
+-module(ofx_tree).
+-include_lib("ofx/include/records.hrl").
+
+-export([
+    get_child/2,
+    get_children/2,
+    get_name/1
+]).
+
+get_child(Name, #ofx_node{children=Children}) ->
+    get_child_list(Name, Children).
+
+get_child_list(_Name, []) ->
+    not_found;
+get_child_list(Name, [Node|Nodes]) ->
+    case get_name(Node) of
+        Name -> Node;
+        _ -> get_child_list(Name, Nodes)
+    end.
+
+get_children(Name, #ofx_node{children=Children}) ->
+    lists:filter(
+      fun(Node) -> get_name(Node) =:= Name end,
+      Children
+    ).
+
+get_name(#ofx_node{name=Name}) -> Name;
+get_name(#ofx_leaf{name=Name}) -> Name.
